65de5e570ad85702a2c0a042002b23a3df2365b2
JOHAN69001 <40177634+JOHAN69001@users.noreply.github.com>
2019-09-25 17:28:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php
+a<?php
 
-$ip = EZ_ISDEV ? '185.81.52.141' : $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR'];
 $geoIp = geoip_record_by_name($ip);
 $regionsJSON = file_get_contents('.' . $assetPath['js'] . '/french-regions-departments.json');
 $regions = json_decode($regionsJSON, true);
@@ -11,17 +11,16 @@ if (!empty($geoIp['city'])) {
   $city = $geoIp['city'];
 }
 
+if ($countryCode == 'FR' && !empty($geoIp['postal_code'])) {
+    $postalCode = substr($geoIp['postal_code'], 0, 2);
+    $deptm = $regions[$postalCode]['name'];
+} 
+
 if (!empty($countryCode) && !empty($regionCode)) {
-    if($countryCode == 'FR' && !empty($geoIp['postal_code'])) {
-        $postalCode = substr($geoIp['postal_code'], 0, 2);
-        $region = $regions[$postalCode]['name'];
-    } else {
-        $region = geoip_region_name_by_code($countryCode, $regionCode);
-    }
-} else {
-    $region = _("À proximité");
+  $region = geoip_region_name_by_code($countryCode, $regionCode);
 }
 
-echo $region, $city;
+
+echo $region, $deptm, $city;
 
 ?>
